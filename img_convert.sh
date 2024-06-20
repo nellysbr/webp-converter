9c776bf9daf15213380a17c048601ddc15d3541d
nellysbr <nelson.melloo@hotmail.com>
2024-06-20 16:41:52 -0300

    feat: convert all images on subfolders

--- img_convert.sh
+++ img_convert.sh
@@ -15,30 +15,33 @@ if [ ! -d "$INPUT_DIR" ]; then
   exit 1
 fi
 
-# Converte todas as imagens PNG e JPEG para WebP
-for IMAGE in "$INPUT_DIR"/*.{png,jpg,jpeg}; do
-  # Verifica se existem arquivos com essas extensões
-  if [ ! -e "$IMAGE" ]; then
-    echo "Nenhum arquivo PNG ou JPEG encontrado em $INPUT_DIR"
-    exit 1
-  fi
-
-  # Obtém o nome do arquivo sem a extensão
-  BASENAME=$(basename "$IMAGE")
-  FILENAME="${BASENAME%.*}"
-
-  # Converte a imagem para WebP no mesmo diretório
-  cwebp "$IMAGE" -o "$INPUT_DIR/$FILENAME.webp"
+# Encontra e converte todas as imagens PNG e JPEG para WebP
+find "$INPUT_DIR" -type f \( -iname "*.png" -o -iname "*.jpg" -o -iname "*.jpeg" \) | while read -r IMAGE; do
+  # Obtém a extensão do arquivo
+  EXT="${IMAGE##*.}"
   
-  # Verifica se a conversão foi bem-sucedida
-  if [ $? -eq 0 ]; then
-    # Exclui a imagem original
-    rm "$IMAGE"
-    # Exibe uma mensagem de sucesso
-    echo "Convertido e deletado: $IMAGE -> $INPUT_DIR/$FILENAME.webp"
-  else
-    # Exibe uma mensagem de erro se a conversão falhar
-    echo "Erro ao converter: $IMAGE"
+  # Verifica se a extensão é válida
+  if [[ "$EXT" =~ ^(png|jpg|jpeg)$ ]]; then
+    # Obtém o nome do arquivo sem a extensão
+    BASENAME=$(basename "$IMAGE")
+    FILENAME="${BASENAME%.*}"
+
+    # Diretório onde a imagem convertida será salva
+    DIRNAME=$(dirname "$IMAGE")
+
+    # Converte a imagem para WebP no mesmo diretório
+    cwebp "$IMAGE" -o "$DIRNAME/$FILENAME.webp"
+    
+    # Verifica se a conversão foi bem-sucedida
+    if [ $? -eq 0 ]; then
+      # Exclui a imagem original
+      rm "$IMAGE"
+      # Exibe uma mensagem de sucesso
+      echo "Convertido e deletado: $IMAGE -> $DIRNAME/$FILENAME.webp"
+    else
+      # Exibe uma mensagem de erro se a conversão falhar
+      echo "Erro ao converter: $IMAGE"
+    fi
   fi
 done
 
